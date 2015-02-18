17dceab95607ae293df4fc2ec7bf26685e417b65
Sanders Lee <slee_colonel@hotmail.com>
2015-02-18 13:18:10 -0800

    Steps 3 and 4 complete, and fixed autoload to load all 3 tables instead of 1

--- application/controllers/Order.php
+++ application/controllers/Order.php
@@ -17,8 +17,15 @@ class Order extends Application {
 
     // start a new order
     function neworder() {
-        //FIXME
-
+        $order_num = $this->orders->highest() + 1;
+        
+        $neworder = $this->orders->create();
+        $neworder->num = $order_num;
+        $neworder->date = date();
+        $neworder->status = 'a';
+        $neworder->total = 0;
+        $this->orders->add($neworder);
+        
         redirect('/order/display_menu/' . $order_num);
     }
 
@@ -30,7 +37,9 @@ class Order extends Application {
         $this->data['pagebody'] = 'show_menu';
         $this->data['order_num'] = $order_num;
         //FIXME
-
+        $this->data['title'] = "Order # " . $order_num . ' (' .
+                number_format($this->orders->total($order_num), 2) . ')';
+        
         // Make the columns
         $this->data['meals'] = $this->make_column('m');
         $this->data['drinks'] = $this->make_column('d');
@@ -63,8 +72,7 @@ class Order extends Application {
     
     // make a menu ordering column
     function make_column($category) {
-        //FIXME
-        return $items;
+        return $this->menu->some('category', $category);
     }
 
     // add an item to an order
