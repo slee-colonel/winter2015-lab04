49e5cfd493ca539ae25fbf097db7f04d028e990a
slee-colonel <slee_colonel@hotmail.com>
2015-02-19 23:39:17 -0800

    Completed steps 5 and 6, but the Proceed button won't disable for some reason even though validation gave the correct value and 'okornot' was set correctly. A possible problem with bootstrap?

--- application/controllers/Order.php
+++ application/controllers/Order.php
@@ -36,7 +36,7 @@ class Order extends Application {
 
         $this->data['pagebody'] = 'show_menu';
         $this->data['order_num'] = $order_num;
-        //FIXME
+        
         $this->data['title'] = "Order # " . $order_num . ' (' .
                 number_format($this->orders->total($order_num), 2) . ')';
         
@@ -77,7 +77,7 @@ class Order extends Application {
 
     // add an item to an order
     function add($order_num, $item) {
-        //FIXME
+        $this->orders->add_item($order_num,$item);
         redirect('/order/display_menu/' . $order_num);
     }
 
@@ -86,8 +86,19 @@ class Order extends Application {
         $this->data['title'] = 'Checking Out';
         $this->data['pagebody'] = 'show_order';
         $this->data['order_num'] = $order_num;
-        //FIXME
-
+        
+        $this->data['total'] = number_format($this->orders->total($order_num), 2);
+        
+        $items = $this->orderitems->group($order_num);
+        foreach ($items as $item)
+        {
+            $menuitem = $this->menu->get($item->item);
+            $item->code = $menuitem->name;
+        }
+        $this->data['items'] = $items;
+        
+        $this->data['okornot'] = $this->orders->validate($order_num);
+        
         $this->render();
     }
 
--- application/views/show_order.php
+++ application/views/show_order.php
@@ -7,7 +7,7 @@
     {code}
 </div>
 {/items}
-<div class="row">
+<div class="row">    
     <a href="/order/commit/{order_num}" class="btn btn-large btn-success {okornot}">Proceed</a>
     <a href="/order/display_menu/{order_num}" class="btn btn-large btn-primary">Keep shopping</a>
     <a href="/order/cancel/{order_num}" class="btn btn-large btn-danger">Forget about it</a>
